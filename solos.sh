ba01f7d820d2685a6e983246997cf9252c02c80b
vukilis <vuklekic22@gmail.com>
2022-01-03 22:07:15 +0100

    commit

--- solos.sh
+++ solos.sh
@@ -5,10 +5,7 @@ echo "------ SolOS installation begun ------"
 echo "--------------------------------------" && sleep 1
 
 bash 0-preinstall.sh
-bash 1-setup.sh
-bash 2-user.sh
-bash 3-post-setup.sh
-# arch-chroot /mnt /root/SolOS/1-setup.sh
-# source /mnt/root/SolOS/install.conf
-# arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/SolOS/2-user.sh
-# arch-chroot /mnt /root/SolOS/3-post-setup.sh
+arch-chroot /mnt /root/SolOS/1-setup.sh
+source /mnt/root/SolOS/install.conf
+arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/SolOS/2-user.sh
+arch-chroot /mnt /root/SolOS/3-post-setup.sh
